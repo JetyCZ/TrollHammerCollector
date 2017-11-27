6c2de716246c38ec758306e98578611f9a05ad69
Pavel Jetensky <pavel.jetensky@hooyu.com>
2017-11-27 14:53:23 +0100

    latest

---
+++ src/test/groovy/net/jetensky/trollhammer/dao/datafactory/Creator.java
@@ -0,0 +1,76 @@
+package net.jetensky.trollhammer.dao.datafactory;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.lang.reflect.FieldUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class Creator implements ApplicationContextAware{
+     /*
+    Log log = LogFactory.getLog(Creator.class);
+
+    private ApplicationContext applicationContext;
+
+    public void save(Object entity) {
+        try {
+            Map props = PropertyUtils.describe(entity);
+            List<Field> allFields = FieldUtils.getAllFieldsList(entity.getClass());
+            for (Field field : allFields) {
+                try {
+                    Object propValue = FieldUtils.readField(field, entity);
+                    saveChildEntity(propValue);
+                } catch (IllegalAccessException e) {
+                    log.info("Skipping " + field.getName() + ", as it is probably private");
+                }
+            }
+
+            for (Object propName : props.keySet()) {
+                Object propValue = props.get(propName);
+                saveChildEntity(propValue);
+            }
+
+            ((JpaRepository) getDao(entity)).save(entity);
+        } catch (Exception e) {
+            throw new IllegalStateException("Problem", e);
+        }
+
+    }
+
+    private void saveChildEntity(Object propValue) {
+        if (propValue!=null)  {
+            Class<?> valueClass = propValue.getClass();
+            if (valueClass.getDeclaredAnnotationsByType(Entity.class).length>0) {
+
+                save(propValue);
+                String className = propValue.getClass().getSimpleName();
+                String daoName = className.substring(0,1).toLowerCase() + className.substring(1) + "DAO";
+
+                JpaRepository jpaRepository = applicationContext.getBeansOfType(JpaRepository.class).get(daoName);
+                jpaRepository.save(propValue);
+            }
+        }
+    }
+
+    private RegionDAO getDao(Object entity) {
+        return regionDAO;
+    }
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+        this.applicationContext = applicationContext;
+    }
+    */
+}
